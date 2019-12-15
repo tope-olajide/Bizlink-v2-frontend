05072a7ba953c04a6dbb63851425d1c05edebe24
Temitope <tope_olajide@outlook.com>
2019-12-15 08:38:39 +0100

    Create HomeAnimation.jsx

---
+++ src/components/Auth/HomeAnimation.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../../styles/cb-slideshow.css';
+
+class HomeAnimation extends React.Component {
+render () {
+    return (
+        <div>
+            <ul className="cb-slideshow">
+<li><span>Image 01</span>
+  <div>
+    <h3>{this.props.caption1}
+    </h3>
+  </div>
+</li>
+<li><span>Image 02</span>
+  <div>
+    <h3>
+    {this.props.caption2}
+    </h3>
+  </div>
+</li>
+<li><span>Image 03</span>
+  <div>
+    <h3>{this.props.caption3} </h3>
+  </div>
+</li>
+<li><span>Image 04</span>
+  <div>
+    <h3>{this.props.caption4} </h3>
+  </div>
+</li>
+<li><span>Image 05</span>
+  <div>
+    <h3>{this.props.caption5}</h3>
+  </div>
+</li>
+<li><span>Image 06</span>
+  <div>
+    <h3>{this.props.caption6}</h3>
+  </div>
+</li>
+</ul>
+        </div>
+    )
+}
+}
+export default HomeAnimation;
