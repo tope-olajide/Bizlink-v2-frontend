65f7bad076b0a843b5dec64e59fe9bef316299c6
Temitope <tope_olajide@outlook.com>
2019-12-24 15:23:30 +0100

    Update Business Reducer

--- src/reducer/business.js
+++ src/reducer/business.js
@@ -1,8 +1,10 @@
 import {
-    FETCH_ALL_BUSINESSES,ADD_BUSINESS
+    FETCH_ALL_BUSINESSES,ADD_BUSINESS,SET_BUSINESS_DETAILS,UPVOTE_BUSINESS,
+    DOWNVOTE_BUSINESS, FOLLOW_USER, UNFOLLOW_USER, ADD_TO_FAVOURITE,REMOVE_FROM_FAVOURITE
   } from '../actions/type'
   const initialState = {
-    allBusinesses: {}
+    allBusinesses: {},
+    businessDetails: {}
 }
 export default (state = initialState, action) => {
     switch (action.type) {
@@ -18,6 +20,80 @@ export default (state = initialState, action) => {
               ...state.allBusinesses, allBusinesses: action.pagedBusiness
             },
           };
+          case SET_BUSINESS_DETAILS:
+      return {
+        ...state, businessDetails: action.businessDetails,
+      };
+    case UPVOTE_BUSINESS:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          business: {
+            ...state.businessDetails.business,
+            upvotes: action.business.upvotes,
+            downvotes: action.business.downvotes
+          }
+        }
+      };
+    case DOWNVOTE_BUSINESS:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          business: {
+            ...state.businessDetails.business,
+            upvotes: action.business.upvotes,
+            downvotes: action.business.downvotes
+          }
+        }
+      };
+    case FOLLOW_USER:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isFollowing: action.isFollowing,
+            followersCount: state.businessDetails.infoCount.followersCount + 1
+          }
+        }
+      };
+    case UNFOLLOW_USER:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isFollowing: action.isFollowing,
+            followersCount: state.businessDetails.infoCount.followersCount - 1
+          }
+        }
+      };
+    case ADD_TO_FAVOURITE:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isUserFavourite: action.isFavourite
+          }
+        }
+      };
+    case REMOVE_FROM_FAVOURITE:
+      return {
+        ...state,
+        businessDetails: {
+          ...state.businessDetails,
+          infoCount: {
+            ...state.businessDetails.infoCount,
+            isUserFavourite: action.isFavourite
+          }
+        }
+      };
         default:
             return state;
         }
